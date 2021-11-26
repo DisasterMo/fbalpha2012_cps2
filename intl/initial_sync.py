184f6226a96ad1acef62a789b316c7b3d2f12a28
DisasterMo <michael.burgardt@gmail.com>
2021-11-26 21:24:24 +0100

    patch

--- intl/initial_sync.py
+++ intl/initial_sync.py
@@ -97,8 +97,12 @@ if __name__ == '__main__':
       with open('intl/download_workflow.py', 'r') as workflow:
          workflow_config = workflow.read()
       workflow_config = workflow_config.replace(
+         "subprocess.run(['python3', 'intl/core_option_translation.py', dir_path, core_name])",
+         "subprocess.run(['python3', 'intl/crowdin_prep.py', dir_path, core_name])"
+      )
+      workflow_config = workflow_config.replace(
          "subprocess.run(['python3', 'intl/initial_sync.py', api_key, core_name])",
-         ""
+         "subprocess.run(['python3', 'intl/crowdin_translation_download.py', api_key, core_name])"
       )
       with open('intl/download_workflow.py', 'w') as workflow:
          workflow.write(workflow_config)
